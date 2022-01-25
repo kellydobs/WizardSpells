ad4d4269f8e2345f78941a7cc144dcae2941872f
Kelly L Dobson <kellyfinolmt@gmail.com>
2022-01-25 09:23:12 -0500

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  <>
+    <header>
+      Header<nav></nav>
+    </header>
+    <div>Main Page Content</div>
+    <footer>Footer</footer>
+  </>;
+}
+
+export default App;
